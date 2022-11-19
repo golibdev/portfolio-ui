2b48456ed267a79f824d9dbd945e504e11afe222
golibdev <gnarzullayev2000@gmail.com>
2022-11-19 15:36:50 +0500

    updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,13 @@
 const mainNav = document.querySelector('.main-nav')
 const burger = document.querySelector('.burger-btn')
+const navLink = document.querySelectorAll('.nav-link')
 
 burger.addEventListener('click', (e) => {
    mainNav.classList.toggle('active')
+})
+
+navLink.forEach(link => {
+   link.addEventListener('click', (e) => {
+      mainNav.classList.remove('active')
+   })
 })
